0db427c259efa1e59b8556ec780d2c162dce9b34
Santiago Mozo <smozo615@gmail.com>
2022-12-21 00:12:35 -0500

    Swagger config
      - Install @nest/swagger
      - Add plugin @nest/swagger in nest-cli.json
      - Add swagger configuration in src/main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Inlaze')
+    .setDescription('Test for Inlaze')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
