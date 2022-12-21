81c283ba2409d71e57470d1bb653c2a077d52b88
Santiago Mozo <smozo615@gmail.com>
2022-12-21 15:00:04 -0500

    Require authentication for message routes
      - Create auth guard
      - Add auth guard to messages controller

--- src/messages/controllers/messages/messages.controller.ts
+++ src/messages/controllers/messages/messages.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 
 import { MessagesService } from '../../services/messages/messages.service';
 import { CreateMessageDto } from 'src/messages/dtos/messages.dto';
+import { AuthGuard } from '../../../guards/auth.guard';
 
 @Controller('messages')
+@UseGuards(AuthGuard)
 export class MessagesController {
   constructor(private messagesService: MessagesService) {}
 
