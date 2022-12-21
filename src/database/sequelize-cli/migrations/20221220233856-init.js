f8c0a1cf376fb2c1e4e7cf7e96c35e9ea78d5ad0
Santiago Mozo <smozo615@gmail.com>
2022-12-20 19:20:31 -0500

    Db migrations config
      - Install sequelize-cli
      - Create .sequelizerc
      - Create src/database/sequelize-cli/ with config for migrations
      - Create first migration: init

---
+++ src/database/sequelize-cli/migrations/20221220233856-init.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      fullName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'full_name',
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('users');
+  },
+};
