2bc0890f944f77af8fc797959a79525b71452edd
Santiago Mozo <smozo615@gmail.com>
2022-12-21 02:49:43 -0500

    Change in the user service signup method
      - Install bcrypt
      - Check if username is in used
      - Hash password before store user data in DB

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import * as bcrypt from 'bcrypt';
 
 import { User } from '../../models/users.model';
 import { SignupDto } from 'src/users/dtos/users.dto';
@@ -9,7 +10,22 @@ export class UsersService {
   constructor(@InjectModel(User) private userModel: typeof User) {}
 
   async signup(data: SignupDto) {
-    const user = await this.userModel.create({ ...data });
+    const { username, password } = data;
+
+    // Check if the username is already in use
+    const isInUsed = await this.userModel.findOne({ where: { username } });
+    if (isInUsed) {
+      throw new ConflictException('invalid username');
+    }
+
+    // Hash password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // Save new user in db
+    const user = await this.userModel.create({
+      ...data,
+      password: hashPassword,
+    });
     return user;
   }
 }
